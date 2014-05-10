e25d18559d825a83666c20ced17456cc13336f1a
martinclsn <martin@clsn.se>
2014-05-10 16:25:00 +0200

    Update 1.installHomebrewFormulae.sh

--- 1.installHomebrewFormulae.sh
+++ 1.installHomebrewFormulae.sh
@@ -1,13 +1,14 @@
 ### INSTALL HOMEBREW ###
-
-# ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
 # Reference: http://brew.sh/
 
-# View what's old
-brew outdated
+#Install
+ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
 
 # Make sure homebrew and formulas are up to date
 brew update 
+
+# View what's old
+brew outdated
 
 # Upgrade everything 
 brew upgrade
