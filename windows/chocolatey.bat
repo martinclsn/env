fd4bf2a678c386ab6e3e2f55e0ee3407503e1001
Martin Claesson <martin@clsn.se>
2015-10-04 22:01:03 +0200

    update

--- windows/chocolatey.bat
+++ windows/chocolatey.bat
@@ -4,15 +4,13 @@ REM @powershell -NoProfile -ExecutionPolicy Bypass -Command "iex ((new-object ne
 
 choco upgrade -y ^
 	7zip chocolatey googlechrome conemu cygwin doublecmd dropbox ^
-	github eclipse flashplayerplugin firefox keepass ^
-	jdk8 nodejs slack spotify ^
+	github flashplayerplugin firefox keepass jdk8 nodejs slack spotify ^
 	skype sublime virtualbox winmerge
 
 REM Extra:
 REM choco upgrade -y freecommander-xe steam vlc python2 pip tortoisehg
-REM set PATH="%PATH%:C:\opt\cygwin;C:\opt\cygwin\bin;C:\opt\jdk\bin;C:\opt\maven\bin;C:\opt\nodejs;C:\opt\npm\bin;C:\opt\chocolatey\bin"
+REM set PATH="%PATH%:C:\opt\cygwin;C:\opt\cygwin\bin;C:\opt\jdk\bin;C:\opt\maven\bin;C:\opt\nodejs;C:\opt\npm\bin;%ChocolateyInstall%\bin"
 
 mkdir C:\opt
 mklink /D "C:\opt\cygwin" "C:\tools\cygwin"
-mklink /D "C:\opt\chocolatey" "C:\ProgramData\chocolatey\bin"
 mklink /D "C:\opt\jdk" "C:\Program Files\Java\jdk1.8.0_60"
