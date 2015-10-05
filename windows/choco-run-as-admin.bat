1ecfc3655e1dc75232a74f752138e0ff173eb2a6
Martin Claesson <martin@clsn.se>
2015-10-06 00:06:27 +0200

    update

--- windows/choco-run-as-admin.bat
+++ windows/choco-run-as-admin.bat
@@ -1,4 +1,6 @@
 REM Install chocolatey. See https://chocolatey.org/
+REM Extra packages to consider: steam vlc python2 pip tortoisehg
+
 @powershell -NoProfile -ExecutionPolicy Bypass -Command ^
 	"iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))"
 
@@ -6,10 +8,6 @@ choco upgrade -y ^
 	7zip chocolatey googlechrome conemu doublecmd dropbox freecommander-xe ^
 	github flashplayerplugin firefox keepass jdk8 nodejs slack spotify ^
 	skype sublime virtualbox winmerge
-
-REM Extra:
-REM choco upgrade -y  steam vlc python2 pip tortoisehg
-REM set PATH="%PATH%:C:\opt\cygwin;C:\opt\cygwin\bin;C:\opt\jdk\bin;C:\opt\maven\bin;C:\opt\nodejs;C:\opt\npm\bin;%ChocolateyInstall%\bin"
 
 mkdir C:\opt
 mklink /D "C:\opt\jdk" "C:\Program Files\Java\jdk1.8.0_60"
--- windows/install-cygwin.bat
+++ windows/install-cygwin.bat
@@ -4,4 +4,4 @@ mkdir C:\opt\bin
 
 C:\opt\bin\setup-x86_64.exe --site="ftp://cygwin.uib.no/pub/cygwin/" --root="C:\opt\cygwin" ^
 	--quiet-mode --no-shortcuts --no-startmenu --no-desktop --local-package-dir %TEMP% ^
-	--packages="bash,coreutils,cygutils,cygwin,dos2unix,grep,gzip,git,subversion,vim,curl,wget,openssh,xinit,less,mercurial,python,tar,vim,which"
+	--packages="bash,coreutils,cygutils,cygwin,curl,dos2unix,grep,git,gzip,less,mercurial,openssh,python,subversion,tar,vim,wget,which,xinit"
