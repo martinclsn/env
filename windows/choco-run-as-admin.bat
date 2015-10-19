f25162d7514a1f6d3acb7fbc01595ee9ac8a0674
Martin Claesson <martin@clsn.se>
2015-10-20 00:26:04 +0200

    update

--- windows/choco-run-as-admin.bat
+++ windows/choco-run-as-admin.bat
@@ -1,13 +1,13 @@
 REM Install chocolatey. See https://chocolatey.org/
-REM Extra packages to consider: steam vlc python2 pip tortoisehg
+REM Extra packages to consider: steam python2 pip tortoisehg
 
 @powershell -NoProfile -ExecutionPolicy Bypass -Command ^
 	"iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))"
 
 choco upgrade -y ^
 	7zip chocolatey googlechrome conemu doublecmd dropbox freecommander-xe ^
-	github flashplayerplugin firefox keepass jdk8 nodejs procexp slack spotify ^
-	skype sublime virtualbox winmerge
+	flashplayerplugin firefox foxitreader keepass jdk8 nodejs procexp qbittorrent ^
+	slack spotify skype sublimetext3 virtualbox vlc winmerge
 
 mkdir C:\opt
 mklink /D "C:\opt\jdk" "C:\Program Files\Java\jdk1.8.0_60"
