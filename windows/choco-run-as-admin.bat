08e4ee92f826c48701c2958dd7bfb328e518697e
Martin Claesson <martin@clsn.se>
2015-10-20 00:42:20 +0200

    update

--- windows/choco-run-as-admin.bat
+++ windows/choco-run-as-admin.bat
@@ -1,11 +1,8 @@
 REM Install chocolatey. See https://chocolatey.org/
-REM Extra packages to consider: steam python2 pip tortoisehg
-
-@powershell -NoProfile -ExecutionPolicy Bypass -Command ^
-	"iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))"
+REM Extra packages to consider: python2 pip tortoisehg
 
 choco upgrade -y ^
-	7zip chocolatey googlechrome conemu doublecmd dropbox freecommander-xe ^
+	7zip chocolatey googlechrome googledrive conemu doublecmd dropbox freecommander-xe ^
 	flashplayerplugin firefox foxitreader keepass jdk8 nodejs procexp qbittorrent ^
 	slack spotify skype sublimetext3 virtualbox vlc winmerge
 
