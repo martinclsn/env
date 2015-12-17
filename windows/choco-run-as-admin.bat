ed12d485975b1e8a496939ea74c20d78aed9a95b
martin.claesson <martin@clsn.se>
2015-12-18 00:08:11 +0100

    update

--- windows/choco-run-as-admin.bat
+++ windows/choco-run-as-admin.bat
@@ -1,9 +1,9 @@
 REM Install chocolatey. See https://chocolatey.org/
-REM Extra packages to consider: python2 pip tortoisehg
+REM Extra packages to consider: python2 pip tortoisehg paint.net
 
 choco upgrade -y ^
-	7zip adobereader autohotkey.portable ccleaner chocolatey conemu googlechrome ^
-	googledrive doublecmd dropbox freecommander-xe flashplayerplugin ^
+	7zip adobereader autohotkey.portable ccleaner chocolatey conemu github googlechrome ^
+	googledrive dropbox freecommander-xe flashplayerplugin ^
 	firefox keepass jdk8 malwarebytes nodejs procexp ^
 	slack spotify skype sublimetext3 virtualbox vlc winmerge
 
