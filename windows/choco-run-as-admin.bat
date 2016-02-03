8dd887967cd7b45ba074837a7d4dfcae8d7c89e8
Martin Claesson <martin.claesson@jeppesen.com>
2016-02-03 16:29:11 +0100

    update

--- windows/choco-run-as-admin.bat
+++ windows/choco-run-as-admin.bat
@@ -1,11 +1,8 @@
 REM Install chocolatey. See https://chocolatey.org/
-REM Extra packages to consider: python2 pip tortoisehg paint.net
+REM Extra packages to consider: python2 pip tortoisehg paint.net github
 
 choco upgrade -y ^
-	7zip adobereader autohotkey.portable ccleaner chocolatey conemu github googlechrome ^
+	7zip adobereader autohotkey.portable ccleaner chocolatey conemu googlechrome ^
 	googledrive dropbox freecommander-xe flashplayerplugin ^
 	firefox keepass jdk8 malwarebytes nodejs procexp ^
 	slack spotify skype sublimetext3 virtualbox vlc winmerge
-
-mkdir C:\opt
-mklink /D "C:\opt\jdk" "C:\Program Files\Java\jdk1.8.0_60"
