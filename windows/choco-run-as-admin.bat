c1c3dfb2975687a3f9efa37729afe561b1a36256
Martin Claesson <martin@clsn.se>
2017-09-30 18:28:12 +0200

    update

--- windows/choco-run-as-admin.bat
+++ windows/choco-run-as-admin.bat
@@ -3,6 +3,5 @@ REM Extra packages to consider: python2 pip tortoisehg paint.net github
 
 choco upgrade -y ^
 	7zip adobereader autohotkey.portable ccleaner chocolatey conemu googlechrome ^
-	dropbox freecommander-xe flashplayerplugin ^
-	firefox keepass malwarebytes nodejs procexp ^
-	slack spotify skype sublimetext3 virtualbox vlc winmerge
+	dropbox freecommander-xe firefox keepass malwarebytes nodejs procexp ^
+	slack spotify skype sublimetext3 virtualbox winmerge
