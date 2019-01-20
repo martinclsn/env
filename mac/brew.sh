7d1e38483af6388357307151be757ca1ce7266b7
Martin Claesson <martin@MacBookPro18.lan>
2019-01-20 21:13:20 +0100

    update mac setup

--- mac/brew.sh
+++ mac/brew.sh
@@ -2,15 +2,18 @@
 # ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 # Make sure homebrew and formulas are up to date
-brew update && brew upgrade brew-cask && brew cleanup && brew cask cleanup
+brew update && brew cleanup
 
 #Install stuff
-brew tap caskroom/cask
-brew install android-sdk bash brew-cask coreutils docker docker-machine maven node 
+#brew cask install java
+#brew cask cleanup
+brew install bash coreutils docker maven node 
 
-brew cask install dropbox double-commander google-chrome google-drive java \
-	libreoffice magicprefs menumeters skype soundflower spotify \
-	sublime-text3 utorrent virtualbox vlc xquartz
+#brew tap caskroom/cask
+#brew install brew-cask
+#brew upgrade brew-cask && brew cask cleanup
+#brew cask install dropbox double-commander google-chrome google-drive java \
+#	libreoffice skype soundflower spotify sublime-text3 virtualbox vlc
 
 # Check problems
 brew doctor
